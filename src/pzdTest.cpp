0be9b1be31dae3899c76c0a7aa689f4757a3d9f1
Scott Lanham <cranky.blonde.coder@gmail.com>
2024-04-01 21:19:23 +1000

    Build framework testing okay.

---
+++ src/pzdTest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	if(argc > 1)
+	{
+		// The first param.
+		//atoi(argv[1]);
+	}
+
+	cout << "*** Pi Zero Dash Test ***\n";
+}
